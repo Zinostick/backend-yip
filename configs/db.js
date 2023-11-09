d798ea05891a88ebd74844856cb12d5aff649561
zinostick <osauzogift@yahoo.com>
2023-11-09 12:16:37 +0100

    my commit

--- configs/db.js
+++ configs/db.js
@@ -4,6 +4,7 @@ import { MongoClient } from 'mongodb';
 
 // Takes the database cluster URI as an argument and creates a new instance of the mongoClient.
 const dbClient = new MongoClient(process.env.MONGO_URI);
+//console.log(dbClient);
 
 /**
  * Connects to the mongo database and returns a userCollection to be used to perform CRUD operations on the users record
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import { userCollection } from './configs/db.js';
+import { ObjectId } from 'mongodb';
 
 // Express
 const app = express();
@@ -73,20 +74,89 @@ app.get('/users', async (request, response, next) => {
  *  Returns a single user if found by it id, else it throws an error
  */
 // Status code 200
-app.get('/users/:id', () => {});
+app.get('/users/:id', async (request, response, next) => {
+  try {
+    const { id } = request.params;
+    if (!ObjectId.isValid(id)) {
+      return response.status(400).json({ error: 'Invalid user ID' });
+    }
+    const singleUser = await userCollection.findOne({ _id: new ObjectId(id) });
+    if (!singleUser) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+    return response.status(200).json(singleUser);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Status code 200
 // FIelds to be updated are within the approved limit. Check FIG 1.0 for approved fields
 // Updates a user by it id and returns a message telling us if is successful or not
 // Throw an error if request was unsuccessful
-app.patch('/users/:id', () => {});
+app.patch('/users/:id', async (request, response, next) => {
+  const { firstname, lastname, email, dob, bio } = request.body;
+  try {
+    const { id } = request.params;
+    if (!ObjectId.isValid(id)) {
+      return response.status(400).json({ error: 'Invalid user ID' });
+    }
+    const singleUser = await userCollection.findOne({
+      _id: new ObjectId(id),
+    });
+    if (!singleUser) {
+      return response.status(404).json({ error: 'User not found' });
+    } else {
+      const updatedUser = await userCollection.findOneAndUpdate(
+        { _id: new ObjectId(id) },
+
+        {
+          $mod: [
+            { $set: firstname },
+            { $set: lastname },
+            { $set: email },
+            { $set: dob },
+            { $set: bio },
+          ],
+        },
+        { returnDocument: 'after' },
+      );
+      return response.status(200).json(updatedUser);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Status 204
 // Delete a user by it id if found, else throw an error
-app.delete('/users/:id', () => {});
+app.delete('/users/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+    if (!ObjectId.isValid(id)) {
+      return response.status(400).json({ error: 'Invalid user ID' });
+    }
+    const singleUser = await userCollection.deleteOne({
+      _id: new ObjectId(id),
+    });
+    if (!singleUser) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+    return response.status(200).json({ message: 'user deleted' });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Delete all users, else throw an error
-app.delete('/users', () => {});
+app.delete('/users', async (request, response, next) => {
+  try {
+    const users = await userCollection.deleteMany();
+    response.status(200).json({ message: 'all users deleted' });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Central error processing middleware
 app.use((error, request, response, next) => {
